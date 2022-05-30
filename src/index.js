d1db78ed688af2cb607db28dacc56f466bd9b8b2
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-05-30 13:30:25 +0200

    Forgot to add context provider to index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import GameContextProvider from './contexts/GameContextProvider';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <GameContextProvider>
+                <App />
+            </GameContextProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
-
 
