7519defa15feed1296c194837ad76b4664ad80e0
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-06-04 22:05:16 +0200

    Added ship counter

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,6 +6,7 @@ const GameBoard = () => {
     const {socket} = useGameContext()
     const [playerBoard, setPlayerBoard] = useState([]);
 	const [opponentBoard, setOpponentBoard] = useState([]);
+    const [shipsLeft, setShipsLeft] = useState(4)
 
     const ships = [
         {
@@ -43,17 +44,19 @@ const GameBoard = () => {
         for (let y=0; y<rows.length; y++ ) {
             for (let x=0; x<cols.length; x++){
                 mBoard.push(<div className="cell" key={`${cols[x]}${rows[y]}`} data-id={`${cols[x]}${rows[y]}`}></div>)
-                eBoard.push(<div  className="cell" onClick={handleClick} key={`${cols[x]}${rows[y]}`} data-id={`${cols[x]}${rows[y]}`}></div>)
-                // mBoard.push(rows[y]+cols[x])
-                // eBoard.push(rows[y]+cols[x])
+                eBoard.push(<div className="cell" onClick={handleClick} key={`${cols[x]}${rows[y]}`} data-id={`${cols[x]}${rows[y]}`}></div>)
             }
         } 
         setPlayerBoard((playerBoard) => [...playerBoard, ...mBoard])    
         setOpponentBoard((opponentBoard) => [...opponentBoard, ...eBoard])    
     }
 
+    console.log(playerBoard)
+
     let takenCords = []
     let occupied = null 
+
+    // playerBoard.findIndex(x => x.id === '45');
 
     const generateShips = () => {
 
@@ -86,32 +89,48 @@ const GameBoard = () => {
 
         socket.emit('user:clicked', clicked);
 
-        if(takenCords.includes(clicked)) {
+        if (takenCords.includes(clicked)) {
             socket.emit("user:reply", clicked, true)
             e.target.className = "hit"
             
             if (clickedIndex <= 1) {
-                ships[0].length--
-            } 
+                ships[0].length-- 
+                if (ships[0].length === 0){
+                    setShipsLeft((prevState) => prevState - 1);
+                }
+            }
 
-            if(clickedIndex >= 2 && clickedIndex <= 3){
+            if (clickedIndex >= 2 && clickedIndex <= 3){
                 ships[1].length--
-            } 
+                if (ships[1].length === 0){
+                    setShipsLeft((prevState) => prevState - 1);
+                } 
+            }
 
             if(clickedIndex >= 4 && clickedIndex <=6){
                 ships[2].length--
+                if (ships[2].length === 0){
+                    setShipsLeft((prevState) => prevState - 1);
+                }
             } 
 
             if(clickedIndex >=7 && clickedIndex <=10){
                 ships[3].length--
+                if (ships[3].length === 0){
+                    setShipsLeft((prevState) => prevState - 1);
+                }
             } 
-        } else {
+
+            } else {
             socket.emit('user:reply', clicked, false)
-            e.target.className = "miss"
-        }
+                e.target.className = "miss"
+            }
 
         console.log(`shots fired at ${clicked}`);
+        
     }
+    console.log(shipsLeft)
+
 
     useEffect(()=>{
         createBoard(yourBoard,enemyBoard)
