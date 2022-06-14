b156686492a61577ab6c6802e2cba1e07b213fd0
robkarrr <93181892+robkarrr@users.noreply.github.com>
2022-06-14 17:58:41 +0200

    Randomize ship positions

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -14,12 +14,6 @@ const GameBoard = () => {
     let myGrid = [...Array(10)].map(e => Array(10).fill(null));
     let enemyGrid = [...Array(10)].map(e => Array(10).fill(null));
 
-    useEffect(()=> {
-        setPlayerBoard(myGrid)
-        setOpponentBoard(enemyGrid)
-        console.log(playerBoard)
-    },[])
-
     // Make a ship with desired length
     const makeShip = (length) => {
         return {
@@ -35,6 +29,32 @@ const GameBoard = () => {
     const ship2 = makeShip(2)
     const ship3 = makeShip(3)
     const ship4 = makeShip(4)
+
+     // Randomize ship poistions and put them on the grid
+    const randomShipPos = (ship) => {
+        let y = Math.floor(Math.random() * 9)
+        let x = Math.floor(Math.random() * (9 - ship.length))
+
+        for(let i = x; i < x + ship.length; i++){
+           myGrid[y][i] = ship
+        }
+    }
+
+    
+    useEffect(()=> {
+        setPlayerBoard(myGrid)
+        setOpponentBoard(enemyGrid)
+        console.log(playerBoard)
+    },[])
+
+
+    useEffect(()=>{
+        randomShipPos(ship1)
+        randomShipPos(ship2)
+        randomShipPos(ship3)
+        randomShipPos(ship4)
+    },[])
+
   
     return (
         <>
