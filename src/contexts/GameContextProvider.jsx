67dd8031f91a5c18ce7ad4ba8c45c464677b5ddb
Hugo C <hcarzborn@gmail.com>
2023-05-14 20:49:39 +0200

    trying

--- src/contexts/GameContextProvider.jsx
+++ src/contexts/GameContextProvider.jsx
@@ -1,34 +1,40 @@
-import { createContext, useContext, useState } from 'react'
-import socketio from "socket.io-client";
+import { createContext, useContext, useState } from 'react';
+import { io } from 'socket.io-client';
 
-const GameContext = createContext()
-const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL);
+const GameContext = createContext();
+
+const socket = io(process.env.REACT_APP_SOCKET_URL, {
+  transports: ['websocket'],
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
 
 export const useGameContext = () => {
-    return useContext(GameContext)
-}
+  return useContext(GameContext);
+};
 
 const GameContextProvider = ({ children }) => {
+  const [fullGame, setFullGame] = useState(false);
+  const [p1, setP1] = useState('');
+  const [p2, setP2] = useState('');
 
-    const [fullGame, setFullGame] = useState(false)
-    const [p1, setP1] = useState('')
-    const [p2, setP2] = useState('')
+  const values = {
+    socket,
+    fullGame,
+    setFullGame,
+    p1,
+    setP1,
+    p2,
+    setP2,
+  };
 
-    const values = {
-        socket,
-        fullGame,
-        setFullGame,
-        p1, 
-        setP1, 
-        p2, 
-        setP2
-    }
+  return (
+    <GameContext.Provider value={values}>
+      {children}
+    </GameContext.Provider>
+  );
+};
 
-    return (
-        <GameContext.Provider value={values}>
-            {children}
-        </GameContext.Provider>
-    )
-}
-
-export default GameContextProvider
+export default GameContextProvider;
